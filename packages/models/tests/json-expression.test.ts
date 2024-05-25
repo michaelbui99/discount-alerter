cb52c5ac61fe95a17570913b202cc6494f88ec7e
Michael Bui <michaelbui99@hotmail.com>
2024-05-25 16:48:49 +0200

    field selector tests

--- packages/models/tests/json-expression.test.ts
+++ packages/models/tests/json-expression.test.ts
@@ -1,6 +1,47 @@
-import { JsonExpressionScanner, TokenType } from '../src/json-expression';
+import {
+    FieldSelector,
+    JsonExpressionScanner,
+    TokenType,
+} from '../src/json-expression';
 
 describe('JSON Expression tests', () => {
+    describe('Field Selector', () => {
+        test('It should return empty result if property does not exist', () => {
+            // Arrange
+            const obj = {
+                ship: {
+                    name: 'Destroyer',
+                    imo: '4242424',
+                },
+            };
+            const selector = new FieldSelector('ship.crew.captain.name');
+
+            // Act
+            const result = selector.select(obj);
+
+            // Assert
+            expect(result.unwrap()).toBeUndefined;
+            expect(result.hasValue()).not.toBeTruthy();
+        });
+
+        test('It should return string result', () => {
+            const obj = {
+                ship: {
+                    name: 'Destroyer',
+                    imo: '4242424',
+                },
+            };
+            const selector = new FieldSelector('ship.name');
+
+            // Act
+            const result = selector.select(obj);
+
+            // Assert
+            expect(result.hasValue()).toBeTruthy();
+            expect(result.unwrapAs<string>()).toEqual('Destroyer');
+        });
+    });
+
     describe('Scanner', () => {
         test('It should parse correct tokens - String literal', () => {
             // Arrange
